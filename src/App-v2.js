9e6d920c6c5e2a7d7ba03d6c4f97aa684f3d2638
Samriddhi Tayal <samriddhitayal@gmail.com>
2023-07-11 23:13:15 +0530

    api key modified

--- src/App-v2.js
+++ src/App-v2.js
@@ -51,7 +51,7 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "f84fc31d";
+const KEY = '8d44062c';
 
 export default function App() {
   const [query, setQuery] = useState("");
